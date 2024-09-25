5ffb57808dc83fa5b6d0523b1e813c4b6a92aa27
Felipe De Andrade <felipe.deam98@gmail.com>
2024-09-25 13:13:03 -0300

    fix: Tasks Done!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine($"Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 1", "1111111111", memoria: 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine($"Smartphone iPhone:");
+Smartphone iphone = new Iphone(numero: "789123", modelo: "Modelo 2", "2222222222", memoria: 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
